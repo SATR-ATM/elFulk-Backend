e7c90ebc4ee2b183a3ac6e72aa3c4875e38d2e44
merzouka <marzoukayouness@gmail.com>
2026-03-06 18:13:19 +0100

    feat(session): Added boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SessionModule } from './modules/session/session.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { AppService } from './app.service';
     }),
     UsersModule,
     AuthModule,
+    SessionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
