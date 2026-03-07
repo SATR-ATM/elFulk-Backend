4622d1f6021df1dbf88d6d7917c5b9f5a2fdbaf4
merzouka <marzoukayouness@gmail.com>
2026-03-07 14:40:13 +0100

    feat(session): Added documentation to DTOs

--- src/modules/session/dto/create-session.dto.ts
+++ src/modules/session/dto/create-session.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNumber, IsOptional, IsUUID } from 'class-validator';
 
 export class CreateSessionDto {
+  @ApiProperty({
+    description: 'UUID of the child user',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
   @IsUUID()
   childId: string;
 
+  @ApiPropertyOptional({
+    description: 'Session duration in ms (defaults to 1 hour)',
+    example: 3600000,
+  })
   @IsNumber()
   @IsOptional()
   endTime?: number;
--- src/modules/session/dto/update-session.dto.ts
+++ src/modules/session/dto/update-session.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber } from 'class-validator';
 
 export class UpdateSessionDto {
+  @ApiProperty({
+    description: 'New duration to add in ms',
+    example: 3600000,
+  })
   @IsNumber()
   endTime: number;
 }
