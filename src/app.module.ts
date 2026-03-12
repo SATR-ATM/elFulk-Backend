c6d48982ca41950e6085adafc94cc61cd953802b
Wala Eddine <135642375+WalaEddine01@users.noreply.github.com>
2026-03-13 00:19:16 +0100

    Configure TypeORM to use PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { AdminModule } from './modules/admin/admin.module';
     }),
 
     TypeOrmModule.forRoot({
+      type: 'postgres',
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USER,
