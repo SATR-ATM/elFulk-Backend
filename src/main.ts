3b59fc4a6df397122205c7e0522fe6e8d858da9a
Touil Khalil <khaliltouil123s@gmail.com>
2026-03-15 18:50:07 +0100

    feat:Swagger/OpenAPI Documentation

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,9 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  app.setGlobalPrefix('api/v1');
+
+  SwaggerModule.setup('api/doc', app, document);
 
   await app.get(AdminService).ensureSuperAdminExists();
 
